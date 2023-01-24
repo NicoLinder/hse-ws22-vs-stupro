cfefe0351ce421f89cba61863a3af692741d0b79
Nico Linder <niliit00@hs-esslingen.de>
2023-01-24 02:59:11 +0100

    Create configurable property

--- service/src/main/java/de/hse/stupro/resources/ProductResource.java
+++ service/src/main/java/de/hse/stupro/resources/ProductResource.java
@@ -2,6 +2,7 @@ package de.hse.stupro.resources;
 
 import de.hse.stupro.dao.ProductDao;
 import de.hse.stupro.model.Product;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -9,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/products")
 public class ProductResource {
@@ -16,11 +18,20 @@ public class ProductResource {
     @Inject
     ProductDao productDao;
 
+    @ConfigProperty(name = "discount.percent", defaultValue = "0")
+    String discountPercent;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Product> getAvailableProducts() {
         var allProducts = productDao.getAllProducts();
-        //return allProducts.stream().filter(product -> product.getStock() > 0).collect(Collectors.toList());
-        return allProducts;
+
+        // Sort out products that are currently not in Stock
+        List<Product> productsInStock = allProducts.stream().filter(product -> product.getStock() > 0).toList();
+
+        // Apply the configured discount
+        int modifier = (100 - Integer.parseInt(discountPercent)) / 100;
+        productsInStock.forEach(product -> product.setPrice(product.getPrice() * modifier));
+        return productsInStock;
     }
 }
